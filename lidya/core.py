31f177e411f816a7da16f42c5fd37bdea781bb15
sunwaterdev <contact.sunwater@gmail.com>
2024-06-21 18:43:55 +0200

    Integration of user recorgnition... & edits

--- lidya/core.py
+++ lidya/core.py
@@ -3,30 +3,19 @@
 # Made by SunWater_
 
 import speech_recognition as sr
-from coqui_tts import TTSCoqui
+import libs.tts as tts
 
-# Mise en place de la reconnaissance vocale
 r = sr.Recognizer()
-tts = TTSCoqui()
+tts = tts.TTS('fr-FR', 'siwis-medium')
 
-# Fonction pour écouter et répéter
 def listen_and_repeat():
     with sr.Microphone() as source:
-        # Enregistrement du message vocal
-        print("Dites quelque chose après le signal 'OK'")
         audio = r.listen(source)
-        print('here')
-        print(r.recognize_google(audio, language='fr-FR'))
-        try:
-            # Reconnaissance vocale du message enregistré
-            text = r.recognize_google(audio, language='fr-FR')
-            print("Vous avez dit: " + text)
-            # Synthèse vocale du texte reconnu
-            tts.synthesize(text, "output.wav")
-        except sr.UnknownValueError:
-            print("Impossible de reconnaître la parole")
-        except sr.RequestError:
-            print("Problème de connexion à l'API de reconnaissance vocale")
+        user_message = r.recognize_google(audio, language='fr-FR')
 
-# Appel de la fonction pour écouter et répéter
+        # Check if message contain "ok lidya"
+        print(user_message)
+        if user_message.split()[0] in ['ok', 'Ok', 'okay', 'Okay'] and user_message.split()[1] in ['Lidya', 'lidya']:
+            pass
+
 listen_and_repeat()
