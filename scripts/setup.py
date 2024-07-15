b47459400f09f6e85dbfd06266528af3f72721f0
Jules <contact.sunwater@gmail.com>
2024-07-15 22:58:25 +0200

    Not finished. Please wait for the feature

--- scripts/setup.py
+++ scripts/setup.py
@@ -27,7 +27,8 @@ CONFIG_URL = "https://pastebin.com/raw/Rmu8qxB9"
 MESSAGES_URL = "https://pastebin.com/raw/4BaVZZS7"
 KEYS_URL = "https://pastebin.com/raw/7uK49WRr"
 WAKEWORDS_URL = "https://pastebin.com/raw/1WGYn2Zk"
-MODELS_URL = "https://pastebin.com/raw/HxxQrcd9"
+TTS_MODELS_URL = "https://pastebin.com/raw/HxxQrcd9"
+STT_MODEL_URL = "https://pastebin.com/raw/NNjDjPWE"
 CONFIG_FILE = "./config/config.json"
 MESSAGES_FILE = "./config/messages.json"
 KEYS_FILE = "./config/keys.json"
@@ -52,7 +53,7 @@ with console.status(" 🌐 Fetching configuration... ",
     MESSAGES = json.loads(requests.get(MESSAGES_URL, timeout=100).text)
     KEYS = json.loads(requests.get(KEYS_URL, timeout=100).text)
     WAKEWORDS = json.loads(requests.get(WAKEWORDS_URL, timeout=100).text)
-    MODELS = json.loads(requests.get(MODELS_URL, timeout=100).text)
+    TTS_MODELS = json.loads(requests.get(TTS_MODELS_URL, timeout=100).text)
 
 dprint('[bold blue][*][/bold blue] 🌐 Online default configuration is ready! ')
 dprint('[bold blue][*][/bold blue] ⌛ Setup wizard is ready! Please complete few questions bellow.')
@@ -82,11 +83,11 @@ dprint('[bold blue][*][/bold blue] 📝 Updating configuration... ')
 CONFIG['main_model'] = OPENAI_FAV_MODEL
 
 # Download model
-AVILABLES_LANGUAGES = MODELS.keys()
+AVILABLES_LANGUAGES = TTS_MODELS.keys()
 USER_LANGUAGE = Prompt.ask('[bold blue][?][/bold blue] 🗣️  What language do you speak best',
                            choices=AVILABLES_LANGUAGES, default="en_US")
-STT_MODEL_URL = MODELS[USER_LANGUAGE]
-with console.status(" 🤖 Fetching your model... You have time to get some fresh air!",
+TTS_MODELS_URL = TTS_MODELS[USER_LANGUAGE]
+with console.status(" 🤖 Fetching your TTS model... You have time to get some fresh air!",
                     spinner="dots12",
                     spinner_style="blue") as progress:
     local_filename = STT_MODEL_URL.split('/')[-1]
@@ -102,7 +103,9 @@ with console.status(" 🤖 Fetching your model... You have time to get some fres
         f.close()
 
     CONFIG['tts_model'] = strip_extension(local_filename, extensions=[".tar.bz2", ".tar.gz"])
-    CONFIG['main_language'] = USER_LANGUAGE
+    
+    
+CONFIG['main_language'] = USER_LANGUAGE
 
 # Write configuration
 dprint('[bold blue][*][/bold blue] 📝 Writing configuration... ')
